47371822e997ceda65319abd405772329042f50d
Donikannerr <67806377+Donikannerr@users.noreply.github.com>
2021-03-21 01:51:04 +0100

    Switch-Animator-Wander-Added

--- Assets/Zombies/ZombieController.cs
+++ Assets/Zombies/ZombieController.cs
@@ -9,6 +9,11 @@ public class ZombieController : MonoBehaviour
     Animator anim;
     public GameObject target;
     NavMeshAgent agent;
+
+    enum STATE {IDLE, WANDER, ATTACK, CHASE, DEAD};
+    STATE state = STATE.IDLE;
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,53 +22,45 @@ public class ZombieController : MonoBehaviour
         anim.SetBool("isWalking", true);
     }
 
+    void TurnOffTriggers()
+    {
+        anim.SetBool("isWalking",false);
+        anim.SetBool("isAttacking",false);
+        anim.SetBool("isRunning",false);
+        anim.SetBool("isDead",false);
+      
+
+    }
     // Update is called once per frame
     void Update()
     {
-
-        agent.SetDestination(target.transform.position);
-        if(agent.remainingDistance > agent.stoppingDistance)
+        switch (state)
         {
-            anim.SetBool("isWalking", true);
-            anim.SetBool("isAttacking", false);
+            case STATE.IDLE:
+                state = STATE.WANDER;
+            break;
+            case STATE.WANDER:
+                if(!agent.hasPath)
+                {
+                    float newX = this.transform.position.x + Random.Range(-5,5);
+                    float newZ = this.transform.position.z + Random.Range(-5,5);
+                    float newY = Terrain.activeTerrain.SampleHeight(new Vector3(newX,0,newZ));
+                    Vector3 dest = new Vector3(newX,newY,newZ);
+                    agent.SetDestination(dest);
+                    agent.stoppingDistance = 0;
+                    TurnOffTriggers();
+                    anim.SetBool("isWalking",true);
+                }
+                
+            break;
+            case STATE.CHASE:
+            break;
+            case STATE.DEAD:
+            break;
         }
-        else
-        {
-            anim.SetBool("isWalking", false);
-            anim.SetBool("isAttacking", true);
-        }
-
-
-        /*
-        if(Input.GetKey(KeyCode.W))
-        {
-            anim.SetBool("isWalking", true);
-        }
-        else
-        {
-            anim.SetBool("isWalking",false);
-        }
+            
         
-        if(Input.GetKey(KeyCode.Mouse0))
-        {
-            anim.SetBool("isAttacking",true);
-        }
-        else
-        {
-            anim.SetBool("isAttacking", false);
-        }
-        if(Input.GetKey(KeyCode.K))
-        {
-            anim.SetBool("isDeath", true);
-        }
-        if(Input.GetKey(KeyCode.LeftShift))
-        {
-            anim.SetBool("isRunning", true);
-        }
-        else
-        {
-            anim.SetBool("isRunning", false);
-        }*/
+        
     }
 
 }
